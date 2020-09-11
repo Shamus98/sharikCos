8e3e6d7b6ec3dfff86904e9a2521adcb11679fa9
Shamus98 <G.ida@yandex.ru>
2020-09-11 17:11:51 +0300

    Update DiffCadr.py

--- DiffCadr.py
+++ DiffCadr.py
@@ -1,23 +1,35 @@
 import cv2
 import numpy as np
 
+def Processing(y):
+    x = cv2.cvtColor(y, cv2.COLOR_BGR2GRAY)
+    x = cv2.GaussianBlur(x,(5, 5), 3)
+    return x
+
 cap = cv2.VideoCapture('./Desktop/Openc/baggage.mp4')
 ret, current_frame = cap.read()
+current_frame = Processing(current_frame)
 previous_frame = current_frame
-while(cap.isOpened()):
-    current_frame_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
-    previous_frame_gray = cv2.cvtColor(previous_frame, cv2.COLOR_BGR2GRAY)    
-    current_frame_gray = cv2.GaussianBlur(current_frame_gray,(5, 5), 3)
-    previous_frame_gray = cv2.GaussianBlur(previous_frame_gray,(5, 5), 3)
-    frame_diff = cv2.absdiff(current_frame_gray,previous_frame_gray)
-    new_frame = cv2.resize(frame_diff,(1400, 720))
-    ret2,th2 = cv2.threshold(new_frame,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    cv2.imshow('frame diff ',th2)      
+m_prev = current_frame
+
+
+
+
+while(cap.isOpened()):    
+    m = cv2.add(cv2.multiply(0.9,m_prev), cv2.multiply(0.1,current_frame))
+    frame_diff = cv2.absdiff(m, current_frame)
+    new_frame = cv2.resize(frame_diff,(128, 128))
+    new_frame2 = cv2.resize(current_frame,(128,128))
+    ret2,th2 = cv2.threshold(new_frame,0,255,cv2.THRESH_BINARY)
+    cv2.imshow('frame diff ',th2)
+    cv2.imshow('video', new_frame2)      
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
     previous_frame = current_frame.copy()
+    m_prev = m
     ret, current_frame = cap.read()
+    current_frame = Processing(current_frame)
     
 
 cap.release()
